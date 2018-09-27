3045e34d5b9d1f0e8715164b9a1893898b2af32b
Greg <tubatica@gmail.com>
2018-09-26 20:55:47 -0600

    starting over

--- src/components/Container/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Container.css"
-
-const Container = props => {
-  console.log(props);
-  return(
-  <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />);
-}
-export default Container;
